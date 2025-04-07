705a73391a3749948811f522f879fc15b282765a
Khaled Hasan <khaled.hasan@brainstation-23.com>
2025-04-07 14:06:36 +0600

    Added nth prime number function

--- basic_program.c
+++ basic_program.c
@@ -16,16 +16,41 @@ int fibonacci(int n) {
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
+// Prime checking function
+int is_prime(int num) {
+    if (num <= 1) return 0;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return 0;
+    }
+    return 1;
+}
+
+// Function to find nth prime number
+int nth_prime(int n) {
+    int count = 0, num = 1;
+    while (count < n) {
+        num++;
+        if (is_prime(num)) {
+            count++;
+        }
+    }
+    return num;
+}
+
 int main() {
     printf("Hello, World!\n");
 
-    // Test the factorial function
+    // Test Factorial function
     int num = 5;
     printf("Factorial of %d is %d\n", num, factorial(num));
 
-    // Test the Fibonacci function
+    // Test Fibonacci function
     int fib_num = 5;
-    printf("Fibonacci of %d is %d\n", fib_num, fibonacci(fib_num));
+    printf("The %dth Fibonacci number is %d\n", fib_num, fibonacci(fib_num));
+
+    // Test nth prime function
+    int prime_num = 5;
+    printf("The %dth prime number is %d\n", prime_num, nth_prime(prime_num));
 
     return 0;
 }
