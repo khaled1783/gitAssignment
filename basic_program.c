c7064375a3c07f4a49184cfdd4771cf83eed406a
Khaled Hasan <khaled.hasan@brainstation-23.com>
2025-04-07 13:18:23 +0600

    Fibonacci sequence added

--- basic_program.c
+++ basic_program.c
@@ -8,12 +8,24 @@ int factorial(int n) {
     return n * factorial(n - 1);
 }
 
+// Fibonacci function
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
 int main() {
     printf("Hello, World!\n");
 
     // Test the factorial function
     int num = 5;
     printf("Factorial of %d is %d\n", num, factorial(num));
+
+    // Test the Fibonacci function
+    int fib_num = 5;
+    printf("Fibonacci of %d is %d\n", fib_num, fibonacci(fib_num));
 
     return 0;
 }
